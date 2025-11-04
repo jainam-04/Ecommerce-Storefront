62d22270a96f16519e482f3e4bd13d9b972503d5
Jainam Rupani <jainamrupani04@gmail.com>
2025-11-04 15:20:09 +0530

    Created repositories for product and category models

---
+++ Ecommerce-StroreFront-Server/src/main/java/com/app/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.app.repositories;
+
+import com.app.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
