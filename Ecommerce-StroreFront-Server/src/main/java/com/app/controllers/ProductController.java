85414e3ce6e1a580faa61c918c0a9d3d3f2c5432
Jainam Rupani <jainamrupani04@gmail.com>
2025-11-04 15:38:05 +0530

    Created get mapping to get all products from database

--- Ecommerce-StroreFront-Server/src/main/java/com/app/controllers/ProductController.java
+++ Ecommerce-StroreFront-Server/src/main/java/com/app/controllers/ProductController.java
@@ -1,10 +1,23 @@
 package com.app.controllers;
 
+import com.app.models.Product;
+import com.app.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-    
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
+    }
 }
--- Ecommerce-StroreFront-Server/src/main/java/com/app/services/ProductService.java
+++ Ecommerce-StroreFront-Server/src/main/java/com/app/services/ProductService.java
@@ -1,8 +1,19 @@
 package com.app.services;
 
+import com.app.models.Product;
+import com.app.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
 
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getAllProducts(){
+        return repository.findAll();
+    }
 }
