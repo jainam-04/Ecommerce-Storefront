980cfcae2de7692d5bb43f6bdf8ba0c108741e9d
Jainam Rupani <jainamrupani04@gmail.com>
2025-11-04 15:29:25 +0530

    Created get mapping to get all categories from database

--- Ecommerce-StroreFront-Server/src/main/java/com/app/controllers/CategoryController.java
+++ Ecommerce-StroreFront-Server/src/main/java/com/app/controllers/CategoryController.java
@@ -1,10 +1,22 @@
 package com.app.controllers;
 
+import com.app.models.Category;
+import com.app.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public List<Category> getAllCategories(){
+        return service.getAllCategories();
+    }
 }
--- Ecommerce-StroreFront-Server/src/main/java/com/app/services/CategoryService.java
+++ Ecommerce-StroreFront-Server/src/main/java/com/app/services/CategoryService.java
@@ -1,8 +1,19 @@
 package com.app.services;
 
+import com.app.models.Category;
+import com.app.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
 
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> getAllCategories(){
+        return repository.findAll();
+    }
 }
